f665c8773ca806de526d0392d43ffe1d24d1ecbd
Annie Wang <anwang911@gmail.com>
2018-03-24 14:56:01 -0400

    Started working on current location

--- Sigma/Views/MapView.swift
+++ Sigma/Views/MapView.swift
@@ -9,13 +9,36 @@
 import MapKit
 import SwiftyJSON
 import UIKit
+import CoreLocation
 
-class MapView: UIView {
+class MapView: UIView, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        let locationManager = CLLocationManager()
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.delegate = self
+//            locationManager.desiredAccuracy = CLLocationAccuracy
+            locationManager.startUpdatingLocation()
+        } else{
+            return
+        }
+    
+        
+        func locationManager(manager:CLLocationManager, didUpdateLocations locations: [CLLocation]){
+            let location = locations[0]
+            let center = location.coordinate
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegionMake(center, span)
+            
+            mapView.setRegion(region, animated: true)
+            mapView.showsUserLocation = true
+        }
         
         SheltersRequest().start { shelters in
             guard let shelters = shelters else {
@@ -24,12 +47,13 @@ class MapView: UIView {
             
             for shelter in shelters {
                 let pin = MKPointAnnotation()
-                
                 let location = CLLocationCoordinate2DMake(shelter.latitude, shelter.longitude)
+                pin.title = shelter.address
                 pin.coordinate = location
                 
                 self.mapView.addAnnotation(pin)
             }
+            
         }
     }
 }
