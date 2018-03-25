f3e166a6f6ba2ade3781334f1d145de9296b8208
Annie Wang <anwang911@gmail.com>
2018-03-25 08:35:10 -0400

    fixed text view in place view controller

--- Sigma/View Controllers/PlaceViewController.swift
+++ Sigma/View Controllers/PlaceViewController.swift
@@ -20,15 +20,16 @@ class PlaceViewController: UIViewController, UITableViewDataSource, UITableViewD
     
     private let dataSource = [
         Section(title: "Open Hours", description: "24/7"),
-        Section(title: "Meal Plan", description: "Breakfast = 5 σ \n• Granola bars \n•Cereal with milk \n• Peanut butter sandwich \n• Applesauce \nLunch = 5 σ \n• Macaroni and cheese \n• Canned tuna and canned peas \n• Beef stew and bread \n• Canned mandarin orange \n• Canned peach \nDinner = 5 σ \n• Canned chicken with canned corn and carrots \n• Fried rice with spam and canned peas and carrots \n• Spaghetti with ground beef and tomato sauce \n• Canned pears \n• Canned pineapple"),
-        Section(title: "Resources", description: "• Water(1/2 gal) = 5 σ \n• Fuel (1 gal) = 10 σ \n• Shower (10 min) = 15 σ \n• Toothbrush = 2 σ \n• Toothpaste = 2 σ \n• Soap = 2 σ \n• Shampoo = 2 σ \n• Towel = 1 σ \n•  Clothing = 2 σ \n•  Sleeping bag = 5 σ"),
-        Section(title: "Tasks", description: "Clear trees from the road (1 hour) = 10 σ \n•Help rebuild houses (1 hour) = 10 σ \n•Clearing the trees from the road (1 hour) = 10 σ")
+        Section(title: "Meal Plan", description: "Breakfast = 5σ\n• Granola bars\n• Cereal with milk\n• Peanut butter sandwich \n• Applesauce\n\nLunch = 5σ\n• Macaroni and cheese\n• Canned tuna and canned peas\n• Beef stew and bread\n• Canned mandarin orange\n• Canned peach\n\nDinner = 5σ\n• Canned chicken with canned corn and carrots\n• Fried rice with spam and canned peas and carrots\n• Spaghetti with ground beef and tomato sauce\n• Canned pears\n• Canned pineapple"),
+        Section(title: "Resources", description: "• Water(1/2 gal) = 5σ\n• Fuel (1 gal) = 10 σ\n• Shower (10 min) = 15σ\n• Toothbrush = 2σ\n• Toothpaste = 2σ\n• Soap = 2σ\n• Shampoo = 2σ\n• Towel = 1σ\n• Clothing = 2σ\n• Sleeping bag = 5σ"),
+        Section(title: "Tasks", description: "• Help out the rescue team in finding people who are trapped (1 hour) = 10σ \n• Help rebuild houses (1 hour) = 10σ \n• Clear trees from the road (1 hour) = 10σ")
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         contentTableView.dataSource = self
+        contentTableView.delegate = self
     }
     
     override func viewDidLayoutSubviews() {
@@ -42,7 +43,7 @@ class PlaceViewController: UIViewController, UITableViewDataSource, UITableViewD
         topBar.layer.shadowPath = topShadowPath.cgPath
     }
     
-    // MARK: Number of rows in particular section
+    // MARK: UITableViewDataSource Methods
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dataSource.count + 1
@@ -51,7 +52,7 @@ class PlaceViewController: UIViewController, UITableViewDataSource, UITableViewD
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch indexPath.row {
         case 0:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "ShelterInfoCell", for: indexPath) as? ItemCell else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "ShelterInfoCell", for: indexPath) as? ShelterInfoCell else {
                 return UITableViewCell()
             }
             
@@ -68,14 +69,28 @@ class PlaceViewController: UIViewController, UITableViewDataSource, UITableViewD
         }
     }
     
-   
-    
-    // MARK: UITableViewDelegate
+    // MARK: UITableViewDelegate Methods
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
         performSegue(withIdentifier: "transactionSegue", sender: self)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.row {
+        case 0: return 112
+        default:
+            let horizontalMargin: CGFloat = 30
+            let verticalMargin: CGFloat = 51
+            
+            let description = dataSource[indexPath.row - 1].description as NSString
+            let boundingSize = CGSize(width: tableView.frame.size.width - horizontalMargin, height: .greatestFiniteMagnitude)
+            let attributes = [NSFontAttributeName: UIFont.systemFont(ofSize: 14)]
+            let size = description.boundingRect(with: boundingSize, options: [.usesLineFragmentOrigin, .usesFontLeading], attributes: attributes, context: nil).size
+            
+            return size.height + verticalMargin
+        }
     }
     
     @IBAction func closeButton(_ sender: UIButton) {
---
+++ Sigma/Views/ShelterInfoCell.swift
@@ -0,0 +1,16 @@
+//
+//  ShelterInfoCell.swift
+//  Sigma
+//
+//  Created by Annie on 3/25/18.
+//  Copyright © 2018 Sigma. All rights reserved.
+//
+
+import UIKit
+
+class ShelterInfoCell: UITableViewCell {
+    
+    @IBOutlet weak var iconView: UIImageView!
+    @IBOutlet weak var infoView: UIView!
+}
+
